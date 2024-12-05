11285ef2affb586f42ab58a538e3a0d7e45acd6d
Kai-nf <lookkaiyang2005@gmail.com>
2024-12-05 21:25:40 +0800

    Add files via upload

---
+++ Viva2.java
@@ -0,0 +1,38 @@
+package viva.pkg2;
+
+public class Viva2 {
+    //create a function format
+    public static String format(int num, int width){
+        //convert int num into string
+        String x = String.valueOf(num);
+        //initialise new string
+        String newString = "";
+        //declare a condition when padding 0(s) is needed
+        if (width>x.length()){
+            //get number of 0(s) needed to be padded
+            int numOfZero = width - x.length();
+            //repeat adding 0(s) into newString for several needed time(s)
+            for (int i = 1; i <= numOfZero; i++)
+                newString += "0";
+            //add num into newString
+            newString += x;
+        return newString;
+        }
+        else
+            //return original string
+            return x;
+    }
+    
+    public static void main(String[] args) {
+        //call method and store the returned value into variables a, b and c
+        String a = format(34, 4);
+        String b = format(34, 5);
+        String c = format(34, 1);
+        
+        //print variables a, b and c
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
